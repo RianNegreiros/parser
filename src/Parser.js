9086a2d2a0e302bef05b283c9bad70d3b009b007
Rian Dos Santos <riannegreiros@gmail.com>
2023-03-06 17:45:36 -0300

    handle statements

--- src/Parser.js
+++ src/Parser.js
@@ -35,8 +35,52 @@ class Parser {
   Program() {
     return {
       type: 'Program',
-      body: this.Literal()
+      body: this.StatementList()
     }
+  }
+
+  // StatementList
+  //   : Statement
+  //   | Statement StatementList
+  //   ;
+
+  StatementList() {
+    const statementList = [this.Statement()];
+
+    while (this._lookahead != null) {
+      statementList.push(this.Statement());
+    }
+
+    return statementList;
+  }
+
+  // Statement
+  // : ExpressionStatement
+  // ;
+
+  Statement() {
+    return this.ExpressionStatement();
+  }
+
+  // ExpressionStatement
+  //   : Expression ';'
+  //   ;
+
+  ExpressionStatement() {
+    const expression = this.Expression();
+    this._eat(';');
+    return {
+      type: 'ExpressionStatement', 
+      expression
+    };
+  }
+
+  // Expression
+  //   : Literal
+  //   ;
+
+  Expression() {
+    return this.Literal();
   }
 
   // Literal
