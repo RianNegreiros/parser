a82531c40f8f8ecb3f4d76f75cb9e72e919d4be5
Rian Dos Santos <riannegreiros@gmail.com>
2023-03-10 16:41:23 -0300

    add math tests

---
+++ __tests__/math-test.js
@@ -0,0 +1,53 @@
+module.exports = test => {
+  // Add two numbers
+  test(`2 + 2;`, {
+    type: 'Program',
+    body: [
+      {
+        type: 'ExpressionStatement',
+        expression: {
+          type: 'BinaryExpression',
+          operator: '+',
+          left: {
+            type: 'NumericLiteral',
+            value: 2
+          },
+          right: {
+            type: 'NumericLiteral',
+            value: 2
+          }
+        }
+      }
+    ]
+  })
+
+  // Nested expressions
+  test(`3 + 2 - 2`, {
+    type: 'Program',
+    body: [
+      {
+        type: 'ExpressionStatement',
+        expression: {
+          type: 'BinaryExpression',
+          operator: '-',
+          left: {
+            type: 'BinaryExpression',
+            operator: '+',
+            left: {
+              type: 'NumericLiteral',
+              value: 3
+            },
+            right: {
+              type: 'NumericLiteral',
+              value: 2
+            },
+          },
+          right: {
+            type: 'NumericLiteral',
+            value: 2
+          }
+        }
+      }
+    ]
+  })
+}
